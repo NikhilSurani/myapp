8a80af027f49b045abd1e773a42c0fa060cbc5be
nikhil surani <nikhil.surani@digi-corp.com>
2018-04-10 14:10:25 +0530

    login and registration with rights credentials

--- models/user.js
+++ models/user.js
@@ -3,19 +3,19 @@ const bcrypt = require('bcryptjs');
 
 // user schema
 var UserSchema = mongoose.Schema({
-    name: {
-        type: string
-    },
+    name:{
+        type: String,
+    },    
     username:{
-        type: string,
+        type: String,
         index: true,
     },
     email: {
-        type: string,
+        type: String,
 
     },
     password: {
-        type: string,
+        type: String,
 
     }
 },{
@@ -24,7 +24,7 @@ var UserSchema = mongoose.Schema({
 
 var User = module.exports = mongoose.model("User", UserSchema);
 
-module.exports.createUser = function(newUser, callback){
+var createUser = function(newUser, callback){
     bcrypt.genSalt(10, function(err, salt) {
         bcrypt.hash(newUser.password, salt, function(err, hash) {
             newUser.password = hash;
@@ -32,4 +32,29 @@ module.exports.createUser = function(newUser, callback){
         });
     });
 
+}
+
+var getUserByUsername =  (username, callback) => {
+    var query = {username : username};
+    User.findOne(query, callback);
+
+};
+
+var comparePassword = (candidatePassword, hash, callback) => {
+    bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
+        if(err) throw err;
+        callback(null, isMatch);
+
+    });
+
+}
+var getUserById = (id, callback) => {
+    User.findById(id, callback);
+}
+
+module.exports = {
+    createUser,
+    getUserByUsername,
+    comparePassword,
+    getUserById,
 }
--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,21 @@
 var express = require('express');
 var router = express.Router();
-var user = require("../models/user.js");
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+var User = require("../models/user.js");
 
 // Register 
-router.get('/register', function(req, res){
+router.get('/register', function (req, res) {
 	res.render('register');
 });
 // Login
-router.get('/login', function(req, res){
+router.get('/login', function (req, res) {
 	res.render('login');
 });
 
 // Register POST
-router.post('/register', function(req, res){
+router.post('/register', function (req, res) {
 	let name = req.body.name;
 	let username = req.body.username;
 	let email = req.body.email;
@@ -28,12 +31,12 @@ router.post('/register', function(req, res){
 	req.checkBody('password2', 'password do not match').equals(req.body.password);
 
 	var errors = req.validationErrors();
-	if(errors){
+	if (errors) {
 		res.render('register', {
 			errors: errors
 		});
 
-	}else {
+	} else {
 		var newUser = new User({
 			name: name,
 			email: email,
@@ -41,15 +44,60 @@ router.post('/register', function(req, res){
 			password: password
 		});
 
-		user.createUser(newUser, (err, user)=> {
-			if(err) throw err; 
+		User.createUser(newUser, (err, user) => {
+			if (err) throw err;
 			console.log(err);
 		});
 
-		req.flash("success_msg", "You are registered and can now login.");		
+		req.flash("success_msg", "You are registered and can now login.");
 		res.redirect('/users/login');
 	}
-	
+
 });
- 
+
+passport.use(new LocalStrategy(
+	function (username, password, done) {
+		User.getUserByUsername(username, function (err, user) {
+			if (err) throw err;
+			if (!user) {
+				return done(null, false, { message: 'Unknown User' });
+			}
+			User.comparePassword(password, user.password, function (err, isMatch) {
+				if (err) throw err;
+				if (isMatch) {
+					return done(null, user);
+				} else {
+					return done(null, flase, { message: 'Invalid password' });
+				}
+
+			});
+
+
+		});
+	}
+));
+
+passport.serializeUser(function (user, done) {
+	done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+	User.getUserById(id, function (err, user) {
+		done(err, user);
+	});
+});
+
+router.post('/login',
+	passport.authenticate('local', { successRedirect: '/', failureRedirect: '/users/login', failureFlash: true }),
+	function (req, res) {
+		res.redirect('/');
+	});
+
+router.get('/logout', function (req, res, next) {
+	req.logout();
+	req.flash("success_msg", "Successfully logged out from session.");
+	res.redirect("/");
+});
+
+
 module.exports = router;
